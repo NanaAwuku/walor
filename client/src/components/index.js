89f4e5f2191879ee91f4292dfecf0a6f6e96cde5
Nana Awuku <apatrickadjei@gmail.com>
2023-10-27 00:27:25 +0000

    new

---
+++ client/src/components/index.js
@@ -0,0 +1,5 @@
+export {default as Sidebar} from './Sidebar'
+export {default as Navbar} from './Navbar'
+export {default as Header} from './Header'
+export {default as Hero} from './Hero'
+export {default as CustomButton} from './CustomButton'
